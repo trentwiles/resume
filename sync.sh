538dd40f0fa0f364689878d7c751d48e0b265806
trentwiles <wilest7@gmail.com>
2025-06-26 09:59:33 -0400

    bash script, still need to test

--- sync.sh
+++ sync.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 REPO_PATH="/home/trent/resume"
-SITE_STORAGE_PATH="/var/www/html/trentwil.es/a/ResumeTrentWiles.pdf"
+REMOTE_RESUME_NAME="ResumeTrentWiles.pdf"
+SITE_STORAGE_PATH="/var/www/html/trentwil.es/a/$REMOTE_RESUME_NAME"
 ENV_PATH=".env"
 
 # 0. Load .env variables
@@ -16,7 +17,20 @@ git pull
 # 2. Compare MD5s
 
 if [ "$(md5sum $SITE_STORAGE_PATH | cut -d ' ' -f 1)" = "$(md5sum trent_in_progress_resume.pdf | cut -d ' ' -f 1)" ]; then
-  echo "Files are identical"
+    echo "No changes made to resume, nothing to do. Goodbye."
 else
-  echo "Files differ"
+    # 3. If the resumes are not the same, copy the resume in this repo over
+    #    to the storage folder of my website 
+    rm -f $SITE_STORAGE_PATH
+    cp trent_in_progress_resume.pdf $SITE_STORAGE_PATH
+
+    # 4. Purge Cloudflare's cache so the newest version of the resume is
+    #    available on the internet
+    curl https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache \
+        -H 'Content-Type: application/json' \
+        -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+        -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+        -d "{\"files\": [\"https://www.trentwiles.com/a/$REMOTE_RESUME_NAME\"]}"
+
+    echo "Purged https://www.trentwiles.com/a/$REMOTE_RESUME_NAME"
 fi
